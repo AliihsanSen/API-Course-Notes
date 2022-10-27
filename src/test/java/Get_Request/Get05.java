4e552cccd98320d7c0dd9913c99b67e099df63e2
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-27 16:53:14 +0300

    27.10.2022

--- src/test/java/Get_Request/Get05.java
+++ src/test/java/Get_Request/Get05.java
@@ -1,7 +1,10 @@
 package Get_Request;
 
 import Base_Url.RestfulBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class Get05 extends RestfulBaseUrl {
 
@@ -12,12 +15,24 @@ public class Get05 extends RestfulBaseUrl {
             User sends get request to the URL
         Then
             Status code is 200
-      And
-         Among the data there should be someone whose firstname is "Johhny" and lastname is "Dear"
+        And
+            Among the data there should be someone whose firstname is "Johhny" and lastname is "Dear"
      */
 
     @Test
     public void get05() {
+
+        // https://restful-booker.herokuapp.com/booking?firstname=Ali&lastname=Şen
+
+        // 1. Set The URL
+        spec.pathParam("first", "booking").
+                queryParams("firstname", "Ali", "lastname", "Şen");
+
+        // 2. Set The Expected Data
+
+        // 3. Send The Request And Get The Response
+        Response response = given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
 
 
     }
