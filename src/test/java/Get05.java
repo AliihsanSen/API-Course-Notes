53ad790c097f630cca4c8b09b3bb1de6554ce00c
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-27 16:36:26 +0300

    27.10.2022

---
+++ src/test/java/Get05.java
@@ -0,0 +1,13 @@
+public class Get05 {
+
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking
+        When
+            User sends get request to the URL
+        Then
+            Status code is 200
+      And
+         Among the data there should be someone whose firstname is "Johhny" and lastname is "Dear"
+     */
+}
