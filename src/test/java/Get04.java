978f01fccf302d03bbed6be395bbfadeef2357bb
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-27 16:09:10 +0300

    27.10.2022

--- src/test/java/Get04.java
+++ src/test/java/Get04.java
@@ -1,2 +1,28 @@
-public class Get04 {
+import org.testng.annotations.Test;
+
+public class Get04 extends JsonplaceholderBaseUrl{
+
+    /*
+        Given
+            https://jsonplaceholder.typicode.com/todos
+        When
+          I send a GET request to the Url
+       And
+          Accept type is "application/json"
+       Then
+           HTTP Status Code should be 200
+       And
+           Response format should be "application/json"
+       And
+           There should be 200 todos
+       And
+           "quis eius est sint explicabo" should be one of the todos title
+       And
+           2, 7, and 9 should be among the userIds
+     */
+
+    @Test
+    public void get04() {
+
+    }
 }
