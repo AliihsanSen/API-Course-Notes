2e4aadaeed1df6a461993aa0981a01fc75261812
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-27 21:10:26 +0300

    27.10.2022

--- src/test/java/Get_Request/Get06.java
+++ src/test/java/Get_Request/Get06.java
@@ -1,36 +1,57 @@
 package Get_Request;
 
 import Base_Url.RestfulBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
 
 public class Get06 extends RestfulBaseUrl {
 
-    /*
-       Given
-           https://restful-booker.herokuapp.com/booking/22
-       When
-           User send a GET request to the URL
-       Then
-           HTTP Status Code should be 200
-       And
-           Response content type is "application/json"
-       And
-           Response body should be like;
-        {
-          "firstname": "Howard",
-       "lastname": "Colque",
-       "totalprice": 111,
-          "depositpaid": true,
-          "bookingdates": {
-       "checkin": "2018-01-01",
-       "checkout": "2019-01-01"
-   },
-    "additionalneeds": "Breakfast"
+/*
+        Given
+            https://restful-booker.herokuapp.com/booking/2325
+        When
+            User send a GET request to the URL
+        Then
+            HTTP Status Code should be 200
+        And
+            Response content type is "application/json"
+        And
+            Response body should be like;
+         {
+    "firstname": "Bradley",
+    "lastname": "Pearson",
+    "totalprice": 132,
+    "depositpaid": false,
+    "bookingdates": {
+        "checkin": "2022-10-27",
+        "checkout": "2022-11-07"
+    },
+    "additionalneeds": "None"
 }
-    */
+     */
 
     @Test
     public void get06() {
+
+        // 1. Set The URL
+        spec.pathParams("firs", "booking", "second", 2325);
+
+        // 2. Set The Expected Data (Put, Post and Patch)
+
+        // 3. Send The Request And Get The Response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        // 4. Do Assertion
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("")
 
 
     }
