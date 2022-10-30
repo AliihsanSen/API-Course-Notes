c827d1c03e00430f7bb2f0398a12cb7d658b918e
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-30 23:35:12 +0300

    30.10.2022

--- src/test/java/Get_Request/Get13.java
+++ src/test/java/Get_Request/Get13.java
@@ -5,6 +5,8 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
@@ -38,12 +40,21 @@ public class Get13 extends ReqresBaseUrl {
         response.prettyPrint();
 
         // 4. Do Assertion
-        // 1)Status code is 200
+        // 1) Status code is 200
         JsonPath jsonPath = response.jsonPath();
         assertEquals(200,response.getStatusCode());
 
-        // 2)Print all pantone_values
+        // 2) Print all pantone_values
         System.out.println(jsonPath.getList("data.pantone_value"));
+
+        // 3) Print all ids greater than 3 on the console
+        List<Integer> ids = jsonPath.getList("data.findAll{it.id>3}.id");
+        System.out.println("ids = " + ids);
+
+        // Assert that there are 3 ids greater than 3
+        assertEquals(3,ids.size());
+
+        // 4) Print all names whose ids are less than 3 on the console
 
 
     }
