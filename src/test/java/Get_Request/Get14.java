d177e95dbcf3b31604a48fe504c1d0ef6b449284
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-30 23:58:54 +0300

    30.10.2022

--- src/test/java/Get_Request/Get14.java
+++ src/test/java/Get_Request/Get14.java
@@ -1,2 +1,30 @@
-package Get_Request;public class Get14 {
+package Get_Request;
+
+public class Get14 {
+
+    // De-Serialization: Json datayı Java objesine çevirme
+    // Serialization: Java objesini Json formatına çevirme.
+    // De-Serialization: Iki türlü yapacağız.
+    // Gson: Google tarafından üretilmiştir.
+    // Object Mapper: Daha popüler...
+
+      /*
+         Given
+            https://jsonplaceholder.typicode.com/todos/2
+        When
+            I send GET Request to the URL
+        Then
+            Status code is 200
+            And "completed" is false
+            And "userId" is 1
+            And "title" is "quis ut nam facilis et officia qui"
+            And header "Via" is "1.1 vegur"
+            And header "Server" is "cloudflare"
+            {
+                "userId": 1,
+                "id": 2,
+                "title": "quis ut nam facilis et officia qui",
+                "completed": false
+            }
+     */
 }
