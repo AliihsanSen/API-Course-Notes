3598b2a79deef8e022687178838566d2253bc059
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-31 00:48:44 +0300

    31.10.2022

--- src/test/java/Get_Request/Get14.java
+++ src/test/java/Get_Request/Get14.java
@@ -45,11 +45,11 @@ public class Get14 extends JsonplaceholderBaseUrl {
         spec.pathParams("first", "todos", "second", 2);
 
         // 2. Set The Expected Data (Put, Post and Patch)
-        Map<String,Object> expectedData = new HashMap<>();
-        expectedData.put("userId",1);
-        expectedData.put("id",2);
-        expectedData.put("title","quis ut nam facilis et officia qui");
-        expectedData.put("completed",false);
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("userId", 1);
+        expectedData.put("id", 2);
+        expectedData.put("title", "quis ut nam facilis et officia qui");
+        expectedData.put("completed", false);
         System.out.println("expectedData = " + expectedData);
 
         // 3. Send The Request And Get The Response
@@ -60,10 +60,10 @@ public class Get14 extends JsonplaceholderBaseUrl {
         assertEquals(200, response.statusCode());
         Map<String, Object> actualData = response.as(HashMap.class);//De-Serialization
         System.out.println("actualData = " + actualData);
-        assertEquals(expectedData.get("userId"),actualData.get("userId"));
-        assertEquals(expectedData.get("id"),actualData.get("id"));
-        assertEquals(expectedData.get("title"),actualData.get("title"));
-        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+        assertEquals(expectedData.get("userId"), actualData.get("userId"));
+        assertEquals(expectedData.get("id"), actualData.get("id"));
+        assertEquals(expectedData.get("title"), actualData.get("title"));
+        assertEquals(expectedData.get("completed"), actualData.get("completed"));
         assertEquals("1.1 vegur", response.header("Via"));
         assertEquals("cloudflare", response.header("Server"));
 
