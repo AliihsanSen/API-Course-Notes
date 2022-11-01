32f9df5a452ab35d0de6e701fc05d8769163ff68
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-01 23:09:01 +0300

    01.11.2022

--- src/test/java/Get_Request/Get_Practice02.java
+++ src/test/java/Get_Request/Get_Practice02.java
@@ -7,5 +7,23 @@ public class Get_Practice02 {
     @Test
     public void test02() {
 
+        String url = "https://reqres.in/api/users";
+        Response response = given().when().get(url);
+        //given().when().get(url) -> request
+        //Response response -> response
+
+        //Header Test
+        response.then().assertThat()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .contentType(ContentType.JSON);
+
+        //Body Test
+        /* idsi 1 olanın datalarınının aşağıdaki gibi olduğunu test ediniz
+             "email": "george.bluth@reqres.in",
+            "first_name": "George",
+            "last_name": "Bluth",
+         */
+
     }
 }
