bc78781dee49a50ad28ae2d716cae8c42e681b88
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-02 13:34:18 +0300

    02.11.2022

--- src/test/java/Test_Data/GoRestTestData.java
+++ src/test/java/Test_Data/GoRestTestData.java
@@ -1,2 +1,26 @@
-package Test_Data;public class GoRestTestData {
+package Test_Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GoRestTestData {
+
+    public Map<String, String> dataKeyMap(String name, String email, String gender, String status) {
+        Map<String, String> dataKeyMap = new HashMap<>();
+        dataKeyMap.put("name", name);
+        dataKeyMap.put("email", email);
+        dataKeyMap.put("gender", gender);
+        dataKeyMap.put("status", status);
+
+        return dataKeyMap;
+    }
+
+    public Map<String, Object> expectedDataMethod(Object meta, Map<String, String> data) {
+
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("meta", meta);
+        expectedData.put("data", data);
+
+        return expectedData;
+    }
 }
