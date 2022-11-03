e6cb95ac8f686bf623218233f083de3ecb465574
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-03 21:07:15 +0300

    03.11.2022

--- src/test/java/Get_Request/Get18.java
+++ src/test/java/Get_Request/Get18.java
@@ -1,4 +1,30 @@
 package Get_Request;
 
+import org.junit.Test;
+
 public class Get18 {
+
+    /*
+    Given
+        https://gorest.co.in/public/v1/users
+    When
+        User send GET Request
+    Then
+        The value of "pagination limit" is 10
+    And
+        The "current link" should be "https://gorest.co.in/public/v1/users?page=1"
+    And
+        The number of users should  be 10
+    And
+        We have at least one "active" status
+    And
+        Niranjan Gupta, Samir Namboothiri and Gandharva Kaul are among the users
+    And
+        The female users are less than or equals to male users
+ */
+
+    @Test
+    public void get18() {
+
+    }
 }
