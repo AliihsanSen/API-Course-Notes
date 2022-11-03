39e24208f47bd1ac131ce9a6cfc3b321936c67e3
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-03 23:04:59 +0300

    03.11.2022

--- src/test/java/Put_Request/Put01.java
+++ src/test/java/Put_Request/Put01.java
@@ -1,4 +1,37 @@
 package Put_Request;
 
-public class Put01 {
+import Base_Url.JsonplaceholderBaseUrl;
+import org.junit.Test;
+
+public class Put01 extends JsonplaceholderBaseUrl {
+
+    /*
+    Given
+     1) https://jsonplaceholder.typicode.com/todos/198
+     2) {
+             "userId": 21,
+             "title": "Wash the dishes",
+             "completed": false
+           }
+    When
+         I send PUT Request to the Url
+    Then
+         Status code is 200
+    And
+         response body is like   {
+                   "userId": 21,
+                   "title": "Wash the dishes",
+                   "completed": false
+                  }
+    */
+
+    @Test
+    public void put01() {
+
+        // 1. Set The URL
+        spec.pathParams("first","todos","second",198);
+
+        // 2. Set The Expected Data ( put, post, patch)
+
+    }
 }
