e9c03f1bc185c4593aeb8fbe3c88b6917c73fa7b
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-03 22:31:31 +0300

    03.11.2022

--- src/test/java/Post_Request/Post01.java
+++ src/test/java/Post_Request/Post01.java
@@ -1,21 +1,22 @@
 package Post_Request;
 
+import Base_Url.JsonplaceholderBaseUrl;
 import org.junit.Test;
 
-public class Post01 {
+public class Post01 extends JsonplaceholderBaseUrl {
 
      /*
     Given
       1) https://jsonplaceholder.typicode.com/todos
-      2)
-   When
-       I send POST Request to the Url
-   Then
-       Status code is 201 {
+      2) {
             "userId": 55,
             "title": "Tidy your room",
             "completed": false
                          }
+   When
+       I send POST Request to the Url
+   Then
+       Status code is 201
    And
        response body is like {
                                "userId": 55,
