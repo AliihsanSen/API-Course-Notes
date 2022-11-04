94d6dd3e322618aae3681bdedffc68e3c3a9b56c
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-04 22:53:25 +0300

    04.11.2022

--- src/test/java/Pojos/BookingDatesPojo.java
+++ src/test/java/Pojos/BookingDatesPojo.java
@@ -5,4 +5,8 @@ public class BookingDatesPojo {
     private String checkin;
     private String checkout;
 
+    public BookingDatesPojo(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
 }
