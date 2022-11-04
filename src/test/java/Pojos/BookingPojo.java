a57890e98e400ccbe113809fced1fb48040f57d5
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-04 23:01:31 +0300

    04.11.2022

--- src/test/java/Pojos/BookingPojo.java
+++ src/test/java/Pojos/BookingPojo.java
@@ -7,4 +7,14 @@ public class BookingPojo {
     private Integer totalprice;
     private Boolean depositpaid;
     private BookingDatesPojo bookingdates;
+
+    public BookingPojo(String firstname, String lastname, Integer totalprice, Boolean depositpaid, BookingDatesPojo bookingdates) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.totalprice = totalprice;
+        this.depositpaid = depositpaid;
+        this.bookingdates = bookingdates;
+    }
+
+
 }
