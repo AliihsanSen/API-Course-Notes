f573af6412bbac08e5f936329f8931813c36683c
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-04 20:41:05 +0300

    04.11.2022

---
+++ src/test/java/Post_Request/Post02.java
@@ -0,0 +1,43 @@
+package Post_Request;
+
+import org.junit.Test;
+
+public class Post02 {
+
+ /*
+   Given
+       1) https://restful-booker.herokuapp.com/booking
+       2) {
+            "firstname": "John",
+            "lastname": "Doe",
+            "totalprice": 11111,
+            "depositpaid": true,
+            "bookingdates": {
+                "checkin": "2021-09-09",
+                "checkout": "2021-09-21"
+             }
+          }
+   When
+       I send POST Request to the Url
+   Then
+       Status code is 200
+       And response body should be like {
+                                           "bookingid": 5315,
+                                           "booking": {
+                                               "firstname": "John",
+                                               "lastname": "Doe",
+                                               "totalprice": 11111,
+                                               "depositpaid": true,
+                                               "bookingdates": {
+                                                   "checkin": "2021-09-09",
+                                                   "checkout": "2021-09-21"
+                                               }
+                                           }
+                                        }
+*/
+
+    @Test
+    public void post02() {
+
+    }
+}
