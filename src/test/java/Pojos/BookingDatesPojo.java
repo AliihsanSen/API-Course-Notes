de18d2e1efa4110ed981775b5cbaf2ff2af67427
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-04 22:55:35 +0300

    04.11.2022

--- src/test/java/Pojos/BookingDatesPojo.java
+++ src/test/java/Pojos/BookingDatesPojo.java
@@ -2,11 +2,19 @@ package Pojos;
 
 public class BookingDatesPojo {
 
+    // 1-) Tüm keyler için private variable'lar oluşturuyoruz.
     private String checkin;
     private String checkout;
 
+    // 2-) Tüm parametrelerle ve parametresiz constructorlar oluşturuyoruz.
     public BookingDatesPojo(String checkin, String checkout) {
         this.checkin = checkin;
         this.checkout = checkout;
     }
+
+    public BookingDatesPojo() {
+    }
+
+    // 3-) Public Getter ve Setter methodlarini oluşturuyoruz.
+
 }
