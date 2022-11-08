91b319751b48d4c8c03774f82fd0d15b1b8c7297
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-08 14:07:53 +0300

    08.11.2022

--- src/test/java/Delete_Request/Delete01.java
+++ src/test/java/Delete_Request/Delete01.java
@@ -1,6 +1,15 @@
 package Delete_Request;
 
 import Base_Url.JsonplaceholderBaseUrl;
+import Utils.ObjectMapperUtils;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
 
 public class Delete01 extends JsonplaceholderBaseUrl {
     /*
@@ -14,21 +23,22 @@ public class Delete01 extends JsonplaceholderBaseUrl {
      */
 
     @Test
-    public void delete01(){
+    public void delete01() {
 
         // Set the Url
-        spec.pathParams("first","todos","second",198);
+        spec.pathParams("first", "todos", "second", 198);
 
         // Set the expected data
-        Map<String,String> expectedData = new HashMap<>();
+        Map<String, String> expectedData = new HashMap<>();
 
         // Send the Request and Get the Response
         Response response = given().spec(spec).when().delete("/{first}/{second}");
+        response.prettyPrint();
 
         // Do Assertion
         Map actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), HashMap.class);
 
-        assertEquals(200,response.getStatusCode());
+        assertEquals(200, response.getStatusCode());
 
         // 1. Yol
         assertEquals(expectedData, actualData);
@@ -37,7 +47,7 @@ public class Delete01 extends JsonplaceholderBaseUrl {
         assertTrue(actualData.isEmpty());
 
         // 3. Yol
-        assertEquals(0,actualData.size());
+        assertEquals(0, actualData.size());
 
     }
 
