c1a99ba480a848c3a5b627f796d12b1fc44b28fb
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-08 12:58:16 +0300

    08.11.2022

--- src/test/java/Post_Request/Post05ObjectMapper_Map.java
+++ src/test/java/Post_Request/Post05ObjectMapper_Map.java
@@ -55,11 +55,11 @@ public class Post05ObjectMapper_Map extends JsonplaceholderBaseUrl {
         HashMap expectedData =   new ObjectMapper().readValue(jsonInString, HashMap.class);
         System.out.println("expectedData = " + expectedData);
 
-        //Send the Request and Get the Response
+        // Send the Request and Get the Response
         Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
         response.prettyPrint();
 
-        //Do Assertion
+        // Do Assertion
         HashMap actualData = new ObjectMapper().readValue(response.asString(),HashMap.class);
         System.out.println("actualData = " + actualData);
 
@@ -67,6 +67,5 @@ public class Post05ObjectMapper_Map extends JsonplaceholderBaseUrl {
         assertEquals(expectedData.get("completed"),actualData.get("completed"));
         assertEquals(expectedData.get("title"),actualData.get("title"));
         assertEquals(expectedData.get("userId"),actualData.get("userId"));
-
     }
 }
