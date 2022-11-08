574d329c46b02aae19d64829b4a14f1a5d040c17
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-08 11:39:38 +0300

    08.11.2022

--- src/test/java/Post_Request/Post04Pojo.java
+++ src/test/java/Post_Request/Post04Pojo.java
@@ -1,8 +1,16 @@
 package Post_Request;
 
+import Base_Url.RestfulBaseUrl;
+import Pojos.BookingDatesPojo;
+import Pojos.BookingPojo;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
 
-public class Post04Pojo {
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Post04Pojo extends RestfulBaseUrl {
     /*
          Given
           1)  https://restful-booker.herokuapp.com/booking
@@ -39,19 +47,20 @@ public class Post04Pojo {
      */
     @Test
     public void post04Pojo(){
-        //Set the Url
+
+        // Set the Url
         spec.pathParam("first","booking");
 
-        //Set the Expected Data
+        // Set the Expected Data
         BookingDatesPojo bookingDatesPojo = new BookingDatesPojo("2021-09-21","2021-12-21");
         BookingPojo expectedData = new BookingPojo("Ali","Can",999,true, bookingDatesPojo,"Breakfast");
         System.out.println("expectedData = " + expectedData);
 
-        //Send the Request and Get the Response
+        // Send the Request and Get the Response
         Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
         response.prettyPrint();
 
-        //Do Assertion
+        // Do Assertion
         BookingResponseBodyPojo actualData = response.as(BookingResponseBodyPojo.class);
         System.out.println("actualData = " + actualData);
 
