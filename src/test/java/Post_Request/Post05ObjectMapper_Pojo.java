ad3c1daa463ca43d0782008bd9d3ee32edc0d15a
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-08 13:34:53 +0300

    08.11.2022

--- src/test/java/Post_Request/Post05ObjectMapper_Pojo.java
+++ src/test/java/Post_Request/Post05ObjectMapper_Pojo.java
@@ -1,4 +1,25 @@
 package Post_Request;
 
-public class Post05ObjectMapper_Pojo {
+import Base_Url.JsonplaceholderBaseUrl;
+
+public class Post05ObjectMapper_Pojo extends JsonplaceholderBaseUrl {
+     /*
+         Given
+           1) https://jsonplaceholder.typicode.com/todos
+           2) {
+                 "userId": 55,
+                 "title": "Tidy your room",
+                 "completed": false
+               }
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like  {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
 }
