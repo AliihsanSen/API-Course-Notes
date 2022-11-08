8cce172f9a829611797fb46acab45803a4c75c3f
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-08 14:07:02 +0300

    08.11.2022

--- src/test/java/Delete_Request/Delete01.java
+++ src/test/java/Delete_Request/Delete01.java
@@ -1,4 +1,44 @@
 package Delete_Request;
 
-public class Delete01 {
+import Base_Url.JsonplaceholderBaseUrl;
+
+public class Delete01 extends JsonplaceholderBaseUrl {
+    /*
+        Given
+            https://jsonplaceholder.typicode.com/todos/198
+        When
+	 		I send DELETE Request to the Url
+	 	Then
+		 	Status code is 200
+		 	And Response body is { }
+     */
+
+    @Test
+    public void delete01(){
+
+        // Set the Url
+        spec.pathParams("first","todos","second",198);
+
+        // Set the expected data
+        Map<String,String> expectedData = new HashMap<>();
+
+        // Send the Request and Get the Response
+        Response response = given().spec(spec).when().delete("/{first}/{second}");
+
+        // Do Assertion
+        Map actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), HashMap.class);
+
+        assertEquals(200,response.getStatusCode());
+
+        // 1. Yol
+        assertEquals(expectedData, actualData);
+
+        // 2. Yol
+        assertTrue(actualData.isEmpty());
+
+        // 3. Yol
+        assertEquals(0,actualData.size());
+
+    }
+
 }
