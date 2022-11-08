5e06d80246f6ecb44d2f6372c5cf3440520c5dc9
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-08 11:21:28 +0300

    08.11.2022

--- src/test/java/Post_Request/Post03Pojo.java
+++ src/test/java/Post_Request/Post03Pojo.java
@@ -33,18 +33,19 @@ public class Post03Pojo extends JsonplaceholderBaseUrl {
     */
     @Test
     public void post03Pojo() {
-        //Set the Url
+
+        // Set the Url
         spec.pathParam("first", "todos");
 
-        //Set the Expected Data
+        // Set the Expected Data
         JsonPlaceHolderPojo expectedData = new JsonPlaceHolderPojo(55, "Tidy your room", false);
         System.out.println("expectedData = " + expectedData);
 
-        //Send the Post Request and Get the Response
+        // Send the Post Request and Get the Response
         Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
         response.prettyPrint();
 
-        //Do Assertion
+        // Do Assertion
         JsonPlaceHolderPojo actualData = response.as(JsonPlaceHolderPojo.class);
         System.out.println("actualData = " + actualData);
 
