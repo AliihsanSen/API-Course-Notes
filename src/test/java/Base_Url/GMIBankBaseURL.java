e3a2b021df25cef0fbb292e62d27010f08fdd63b
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-09 11:11:31 +0300

    09.11.2022

---
+++ src/test/java/Base_Url/GMIBankBaseURL.java
@@ -0,0 +1,16 @@
+package Base_Url;
+
+import Utils.Authentication;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class GMIBankBaseURL extends Authentication {
+
+    protected RequestSpecification spec01;
+
+    @Before
+    public void setUp(){
+        spec01 = new RequestSpecBuilder().setBaseUri("https://www.gmibank.com/api/").build();
+    }
+}
