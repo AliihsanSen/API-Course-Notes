fe61a0ee3018ad7f1c4a1b416452cae7146a649c
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-10 23:13:19 +0300

    10.11.2022

--- src/test/java/Pojos/DummyRestApiDeletePojo.java
+++ src/test/java/Pojos/DummyRestApiDeletePojo.java
@@ -1,4 +1,14 @@
 package Pojos;
 
 public class DummyRestApiDeletePojo {
+    /*
+    "status": "success",
+    "data": "2",
+    "message": "Successfully! Record has been deleted"
+     */
+    private String status;
+    private String data;
+    private String message;
+
+
 }
