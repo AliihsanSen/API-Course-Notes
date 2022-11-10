446adcd02636f06b4a56f36a5e6a866d680e31f9
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-10 23:43:27 +0300

    10.11.2022

--- src/test/java/Put_Request/Put02.java
+++ src/test/java/Put_Request/Put02.java
@@ -1,4 +1,31 @@
 package Put_Request;
 
-public class Put02 {
+import Base_Url.DummyRestApiBaseUrl;
+
+public class Put02 extends DummyRestApiBaseUrl {
+
+    /*
+        URL: https://dummy.restapiexample.com/api/v1/update/21
+       HTTP Request Method: PUT Request
+       Request body: {
+                        "employee_name": "Ali Can",
+                        "employee_salary": 111111,
+                        "employee_age": 23,
+                        "profile_image": "Perfect image"
+                     }
+       Test Case: Type by using Gherkin Language
+       Assert:
+                i) Status code is 200
+                ii) Response body should be like the following
+                    {
+                        "status": "success",
+                        "data": {
+                            "employee_name": "Ali Can",
+                            "employee_salary": 111111,
+                            "employee_age": 23,
+                            "profile_image": "Perfect image"
+                        },
+                        "message": "Successfully! Record has been updated."
+                    }
+     */
 }
