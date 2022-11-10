223ce9abd0a8decefe8f6c447f1a6b7583ac04f0
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-11 00:07:58 +0300

    10.11.2022

--- src/test/java/Get_Request/Get24.java
+++ src/test/java/Get_Request/Get24.java
@@ -1,4 +1,29 @@
 package Get_Request;
 
-public class Get24 {
+import Base_Url.DummyRestApiBaseUrl;
+import org.junit.Test;
+
+public class Get24 extends DummyRestApiBaseUrl {
+    /*
+    Given
+        URL: https://dummy.restapiexample.com/api/v1/employee/1
+    When
+        User sends GET Request
+    Then
+        Status code is 200
+    And
+        "employee_name" is "Tiger Nixon"
+    And
+        "employee_salary" is 320800
+    And
+        "employee_age" is 61
+    And
+        "status" is "success"
+    And
+        "message" is "Successfully! Record has been fetched."
+     */
+
+    @Test
+    public void get24() {
+    }
 }
