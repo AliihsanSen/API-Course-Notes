b7b3d12e3e49ee6ab62f9bce1a7c4c1be9c3ed5a
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-10 23:42:16 +0300

    10.11.2022

--- src/test/java/Delete_Request/Delete02.java
+++ src/test/java/Delete_Request/Delete02.java
@@ -7,6 +7,7 @@ import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Delete02 extends DummyRestApiBaseUrl {
 
@@ -39,15 +40,18 @@ public class Delete02 extends DummyRestApiBaseUrl {
 
         spec.pathParams("first", "delete", "second", 2);
 
-        DummyRestApiDeletePojo expectedData = new DummyRestApiDeletePojo("success","2","Successfully! Record has been deleted");
+        DummyRestApiDeletePojo expectedData = new DummyRestApiDeletePojo("success", "2", "Successfully! Record has been deleted");
         System.out.println("expectedData = " + expectedData);
 
         Response response = given().spec(spec).when().delete("/{first}/{second}");
         response.prettyPrint();
 
-        DummyRestApiDeletePojo actualData = ObjectMapperUtils.convertJsonToJava(response.asString(),DummyRestApiDeletePojo.class);
+        DummyRestApiDeletePojo actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), DummyRestApiDeletePojo.class);
         System.out.println("actualData = " + actualData);
 
+        assertEquals(expectedData.getData(), actualData.getData());
+        assertEquals(expectedData.getStatus(), actualData.getStatus());
+        assertEquals(expectedData.getMessage(), actualData.getMessage());
 
 
     }
