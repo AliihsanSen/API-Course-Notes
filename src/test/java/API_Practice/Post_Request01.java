05701d066fe8aed1a7080a8ca08e60c0fc4e2d9b
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-10 13:37:49 +0300

    10.11.2022

--- src/test/java/API_Practice/Post_Request01.java
+++ src/test/java/API_Practice/Post_Request01.java
@@ -1,4 +1,36 @@
 package API_Practice;
 
-public class Post_Request01 {
+import Base_Url.GMIBankBaseURL;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Post_Request01 extends GMIBankBaseURL {
+
+    /*
+    https://www.gmibank.com/api/tp-countries adrresine yeeni bir ülke ekelyin
+    */
+
+    @Test
+    public void test10(){
+        spec01.pathParam("first", "tp-countries");
+
+        CountryPost countryPost = new CountryPost("Batch81");
+        System.out.println("countryPost = " + countryPost);
+
+        Response response = given()
+                .headers("Authorization", "Bearer " + generateToken(), "Content-Type", ContentType.JSON)
+                .spec(spec01).when().body(countryPost)
+                .post("/{first}");
+
+        response.prettyPrint();
+
+        CountryPost actualData = response.as(CountryPost.class);
+        System.out.println("actualData = " + actualData);
+
+        // Doğrulama Yaptık
+        Assert.assertEquals(countryPost.getName(), actualData.getName());
+    }
 }
